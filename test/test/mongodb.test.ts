30cb28b93eedbd5d3334e225ca7b18f25ebcdf0a
ec965 <22567131+ec965@users.noreply.github.com>
2022-08-03 23:15:24 -0700

    Fmt

--- test/test/mongodb.test.ts
+++ test/test/mongodb.test.ts
@@ -1,31 +1,29 @@
 import { MongoClient } from "mongodb";
-import { describe, it, expect } from "vitest";
+import { it, expect } from "vitest";
 
-describe("Vitest MongoDB", () => {
-  it("connects to mongodb", () => {
-    expect(async () => {
-      const client = new MongoClient(globalThis.__MONGO_URI__);
-      try {
-        const db = client.db("test");
-        await db.command({ ping: 1 });
-      } finally {
-        await client.close();
-      }
-    }).not.toThrow();
-  });
-
-  it("inserts and reads a document", async () => {
+it("connects to mongodb", () => {
+  expect(async () => {
     const client = new MongoClient(globalThis.__MONGO_URI__);
-    await client.connect();
-    const db = client.db("test");
-    const users = db.collection("users");
-    await users.insertOne({ username: "test123", password: "test123" });
-    const user = await users.findOne({ username: "test123" });
-    await client.close();
+    try {
+      const db = client.db("test");
+      await db.command({ ping: 1 });
+    } finally {
+      await client.close();
+    }
+  }).not.toThrow();
+});
 
-    expect(user).toHaveProperty("username");
-    expect(user).toHaveProperty("password");
-    expect(user?.username).toBe("test123");
-    expect(user?.password).toBe("test123");
-  });
+it("inserts and reads a document", async () => {
+  const client = new MongoClient(globalThis.__MONGO_URI__);
+  await client.connect();
+  const db = client.db("test");
+  const users = db.collection("users");
+  await users.insertOne({ username: "test123", password: "test123" });
+  const user = await users.findOne({ username: "test123" });
+  await client.close();
+
+  expect(user).toHaveProperty("username");
+  expect(user).toHaveProperty("password");
+  expect(user?.username).toBe("test123");
+  expect(user?.password).toBe("test123");
 });
